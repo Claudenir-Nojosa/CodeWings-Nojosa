afe15e5a60662134b86495d5bc57e5863cb26891
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-10-25 20:42:57 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper>
+      <Button variant="outline">Clique aqui</Button>
+    </MaxWidthWrapper>
+  );
+}
